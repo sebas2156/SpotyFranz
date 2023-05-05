3e0ba804bccc5bce581bce898f00a1fc68409b45
sebas2156 <sebastianmoraless257@gmail.com>
2023-05-05 08:42:29 -0400

    version 5/05/23

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ class Cancion:
         self.idioma = idioma
 
 app = Flask (__name__)
-
+cancion1= Cancion('La guitarra','Pop','Castellano')
+cancion2= Cancion('Para no verte mas','Pop','Castellano')
+cancion3= Cancion('Balada para un gordo','Balada','Castellano')
+lista = [cancion1, cancion2, cancion3]
 @app.route('/')
 
 def index():
     
-    cancion1= Cancion('La guitarra','Pop','Castellano')
-    cancion2= Cancion('Para no verte mas','Pop','Castellano')
-    cancion3= Cancion('Balada para un gordo','Balada','Castellano')
-    lista = [cancion1, cancion2, cancion3]
+
     return render_template('listar.html', titulo='Canciones', musicas=lista)
 
 @app.route('/nuevoregistro')
@@ -29,6 +29,6 @@ def crear():
     idioma = request.form['idioma']
     cancion = Cancion(titulo, categoria, idioma)
     lista.append(cancion)
-    return render_template('lista6.html', titulo='Canciones', musicas=lista)
+    return render_template('listar.html', titulo='Canciones', musicas=lista)
 
 app.run(host="0.0.0.0", port=5000)
