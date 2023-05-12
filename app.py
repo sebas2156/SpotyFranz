4435001a71df354b17e09c669e84e7769c37665a
sebas2156 <sebastianmoraless257@gmail.com>
2023-05-12 09:53:20 -0400

    version 12/05/23

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 class Cancion:
     def __init__(self,titulo,categoria,idioma):
@@ -7,6 +7,8 @@ class Cancion:
         self.idioma = idioma
 
 app = Flask (__name__)
+app.secret_key = '12345678'
+
 cancion1= Cancion('La guitarra','Pop','Castellano')
 cancion2= Cancion('Para no verte mas','Pop','Castellano')
 cancion3= Cancion('Balada para un gordo','Balada','Castellano')
@@ -20,7 +22,12 @@ def index():
 
 @app.route('/nuevoregistro')
 def nuevoregistro():
-    return render_template('nuevoRegistro.html',titulo='Nueva Cancion')
+    if session['usuario_logueado'] == None:
+        flash('Usuario no conectado.')
+        return redirect('/login')
+    else:
+        flash('Usuario conectado.')
+        return render_template('nuevoRegistro.html', titulo='Nueva Canción')
 
 @app.route('/crear', methods=['POST',])
 def crear():
@@ -29,6 +36,29 @@ def crear():
     idioma = request.form['idioma']
     cancion = Cancion(titulo, categoria, idioma)
     lista.append(cancion)
-    return render_template('listar.html', titulo='Canciones', musicas=lista)
+    return redirect('/')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'patitofeo' == request.form['clave']:
+        session['usuario_logueado'] = request.form['usuario']
+        flash(session['usuario_logueado'] + ' ¡conectado con éxito!')
+        return redirect('/')
+    else:
+        flash('Usuario no conectado.')
+        return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    session['usuario_logueado'] = None
+    flash('¡Logout efectuado exitosamente!')
+    return redirect('/')
+
+
 
 app.run(host="0.0.0.0", port=5000)
